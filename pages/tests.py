1fda487575d3fb644e45eab4b95676dfd22b89a5
Your Name <ahmetyildirir1@gmail.com>
2022-05-06 21:12:54 +0300

    gonder
    gonder

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
 
-# Create your tests here.
+
+from django.test import SimpleTestCase
+
+
+class SimpleTests(SimpleTestCase):
+    
+    def test_home_page(self):
+        response = self.client.get('/')
+        print(response.status_code)
+        self.assertEqual(response.status_code, 200)
+        
+    def test_about_page(self):
+            response = self.client.get('/about/')
+            print(response.status_code)
+            self.assertEqual(response.status_code, 200)
