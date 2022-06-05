2136a6f77164c05206f362bbd302b49f9ee59840
Ahmetyldrr <60220232+Ahmetyldrr@users.noreply.github.com>
2022-06-05 14:21:07 +0300

    urls veri ekleme

--- pages_project/urls.py
+++ pages_project/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+'deneme eklemesi'
+
 from django.contrib import admin
 from django.urls import path, include # new
 
